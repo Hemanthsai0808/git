8d0f64721e89808da6503face9aa7da52efaa831
Hemanth Sai <hemanthsai0808@gmail.com>
2023-06-09 17:38:45 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<iostream>
+#define int   long long int 
+#define endl  "\n"
+using namespace std;
+
+
+void Bingo()
+{
+    cout << "Hello" << endl;
+     
+}
+
+int32_t main()
+{
+ ios::sync_with_stdio(false);
+ cin.tie(0); cout.tie(0);
+ 
+ int TT=1;
+ //cin >> TT;
+ while(TT--)
+ {
+    Bingo();
+ }
+ return 0;
+}
